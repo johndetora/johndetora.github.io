bde2a0d1d432c8036b825a3d22c6db4d64d09935
JD Win <johndetora@gmail.com>
2021-05-12 14:19:54 -0400

    windows

--- asciitone/scripts/main.js
+++ asciitone/scripts/main.js
@@ -7,11 +7,11 @@ synthParamController();
 let OSName = 'Unknown OS';
 if (navigator.appVersion.indexOf('Win') != -1) OSName = 'Windows';
 if (navigator.appVersion.indexOf('Mac') != -1) OSName = 'MacOS';
-else OSName = 'Linux (probably)';
+// else OSName = 'Linux (probably)';
 console.log('Your OS: ' + OSName);
 const overlay = document.querySelector('.overlay');
 if (navigator.appVersion.indexOf('Win') != -1) {
-    overlay.style.left = '-80px';
+    overlay.style.left = '-120px';
 }
 
 // ------------------------- //
