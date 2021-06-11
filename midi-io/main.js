be6a5ce03145de3f85c794b4295e699ccfa71d4e
John <johndetora@gmail.com>
2021-06-10 22:41:32 -0400

    refactor

--- midi-io/main.js
+++ midi-io/main.js
@@ -2,24 +2,29 @@
 // TODO: Add to midibug along with different viewing options
 // TODO: ADVANCED: add library of device inquiry responses to automatically detect the product
 
-//TODO: easier to read with this seperate, or just add it to the object
-const deviceID = [0x47, 0x7f, 0x4d];
+// Enable webMidiJs
+WebMidi.enable(function (err) {
+    // If midi ports aren't found, log error
+    if (err) console.log('WebMidi could not be enabled.', err);
+    // Otherwise log available midi ports
+    console.log(WebMidi.inputs);
+    console.log(WebMidi.outputs);
+    const input = WebMidi.getInputByName('LPK25 mk2');
+    const output = WebMidi.getOutputByName('LPK25 mk2');
+    receiveSysex(input);
+    sendSysex(output);
+}, true);
 
 // Store messages here
 // TODO: gather this from excel sheet, use regex to cancel out first F0 and last F7
-
 const sysexMessages = {
     velocityCurve: [0x08, 0x00, 0x00],
     // expected: F0 47 7F 4D 08 00 18 00 01 01 19 4C 10 33 19 19 4C 65 26 5C 13 32 66 32 41 6B 41 5A 7F 7F 7F F7 (32 Bytes)
-
-    //[0xF0, 0x7E, 0x00, 0x06, 0x01, 0xF7]
-    // [240, 126, 0, 6, 1, 247],
-    //[126, 0, 6, 1]
-    // Note this has to be decimal due to how the midijs library handles sysex messages.  When sending this message, we are using just the web midi api
-
     deviceInquiry: [0x7e, 0x00, 0x06, 0x01, 0xf7],
-    // deviceInquiry: [0xf0, 0x7e, 0x00, 0x06, 0x01, 0xf7],
 };
+
+//TODO: add this to object or nah?
+const deviceID = [0x47, 0x7f, 0x4d];
 // Get selected sysex
 const submit = document.getElementById('send-sysex');
 const selectionID = document.getElementById('sysex-options');
@@ -28,21 +33,6 @@ function getSelection() {
     const msgKey = selectionID.value;
     return sysexMessages[msgKey];
 }
-
-// TODO: seperate enable to it's own function
-// TODO: refactor some of these functions if needed
-WebMidi.enable(function (err) {
-    // If midi ports aren't found, log error
-    if (err) console.log('WebMidi could not be enabled.', err);
-    // Otherwise log available midi ports
-    console.log(WebMidi.inputs);
-    console.log(WebMidi.outputs);
-    const input = WebMidi.getInputByName(name);
-    const output = WebMidi.getOutputByName(name);
-
-    receiveSysex(input);
-    sendSysex(output);
-}, true);
 
 // Send sysex
 function sendSysex(output) {
@@ -87,52 +77,3 @@ function hexConversion(arr) {
 //     //     const hex = dec.toString(16);
 //     //     console.log('Sysex response is: ', hex);
 // }
-
-// https://stackoverflow.com/questions/57803/how-to-convert-decimal-to-hexadecimal-in-javascript
-
-// const midi = {
-//     input: null,
-//     output: null,
-// };
-
-// let input = null;
-// let output = null;
-
-// setTimeout(() => {
-//     console.log(input);
-// }, 1000);
-// // TODO: seperate enable to it's own function
-// // TODO: refactor some of these functions if needed
-// WebMidi.enable(function (err) {
-//     if (err) {
-//         console.log('WebMidi could not be enabled.', err);
-//     }
-//     // Viewing available inputs and outputs
-//     console.log(WebMidi.inputs);
-//     console.log(WebMidi.outputs);
-
-//     // TODO: put I/O names into function so we can change it later based on the product
-//     const output = WebMidi.getOutputByName('LPK25 mk2');
-//     const input = WebMidi.getInputByName('LPK25 mk2');
-
-//     // Listen for a 'note on' message on all channels
-//     input.addListener('noteon', 'all', function (e) {
-//         console.log("Received 'noteon' message (" + e.note.name + e.note.octave + ').');
-//     });
-
-//     // Send sysex
-//     submit.addEventListener('click', () => {
-//         console.log(selectionID.value + ' message sent');
-//         if (selectionID.value === 'deviceInquiry') output.send(getSelection());
-//         output.sendSysex(deviceID, getSelection());
-//     });
-
-//     // Receive Sysex
-//     input.addListener('sysex', 'all', function (e) {
-//         //TODO: add a conditional to check if a response is expected and if successful
-//         const response = [...e.data];
-//         //TODO: make sure this actually means the message was received
-//         console.log('message received', response);
-//         hexConversion(response);
-//     });
-// }, true);
